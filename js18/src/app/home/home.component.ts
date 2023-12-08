49390505048de2a1179a3474ba56ef214c64e32e
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2023-12-08 13:54:04 +0100

    added bootstrap

--- js18/src/app/home/home.component.ts
+++ js18/src/app/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  count:number = 0;
+  title = 'Xmas Shopping';
 
+  myContent:string = '';
+
+  increaseCount() {
+    this.count++; //gebruik van 'this' verplicht want we zitten in een class!
+  }
 }
