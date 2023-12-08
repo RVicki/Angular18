31f5e92bc26e454c8f75533373c250d5e7e8a1fb
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2023-12-08 14:33:58 +0100

    added fetch methode and restapi call ngOnInit

--- js18/src/app/home/home.component.ts
+++ js18/src/app/home/home.component.ts
@@ -8,13 +8,35 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent { //always put everything in the class, not outside of it
   count:number = 0;
   title = 'Xmas Shopping';
-
   myContent:string = '';
+  url:string = 'http://localhost:3000/todo';
+  todos:any[] = []; //it's available now
 
   increaseCount() {
     this.count++; //gebruik van 'this' verplicht want we zitten in een class!
+  }
+  
+/* fetchMyData() {
+  fetch(this.url)
+  .then(response => response.json())
+  .then(json => console.log(json))
+} 
+
+Underneath is the next step after adding url:string ... above under class*/
+
+  fetchMyData() {
+  fetch(this.url)
+  .then(response => response.json())
+  .then(json => this.todos = json)
+}
+
+  //al de gegevens in db.json invoegen! Moet erbij de volgende ngOnInit!
+  ngOnInit() { 
+    /* console.log('yay, my init works!')  --> test*/
+
+    this.fetchMyData();
   }
 }
