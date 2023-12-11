7d0d626b1c1bf1e04b1e8ecd94c751a5ecd12220
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2023-12-11 14:36:17 +0100

    Dropdown data is taken from db

--- js18/src/app/home/home.component.ts
+++ js18/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -13,11 +14,17 @@ export class HomeComponent { //always put everything in the class, not outside o
   title = 'Xmas Shopping';
   myContent:string = '';
   url:string = 'http://localhost:3000/todo';
+  urlUsers:string = 'http://localhost:3000/users';
   todos:any[] = []; //it's available now
-  owner:string = '';
+  users:any[] = [];
+  user: any;
 
   increaseCount() {
     this.count++; //gebruik van 'this' verplicht want we zitten in een class!
+    localStorage.setItem('count', this.count.toString());
+  }
+  DecreaseCount() {
+    this.count--; //gebruik van 'this' verplicht want we zitten in een class!
     localStorage.setItem('count', this.count.toString());
   }
 
@@ -28,7 +35,7 @@ export class HomeComponent { //always put everything in the class, not outside o
       headers: {'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.3.0'},
       body: JSON.stringify({
         'title': this.myContent,
-        'owner': this.owner,
+        'owner': this.user,
         'done': false
       })
     };
@@ -59,12 +66,18 @@ Underneath is the next step after adding url:string ... above under class*/
     .then(json => this.todos = json)
 }
 
+fetchMyUsers() {
+  fetch(this.urlUsers)
+  .then(response => response.json())
+      .then(json => this.users = json)
+}
 
   //al de gegevens in db.json invoegen! Moet erbij de volgende ngOnInit!
   ngOnInit() { 
     /* console.log('yay, my init works!')  --> test*/
 
     this.fetchMyData();
+    this.fetchMyUsers();
   }
 
   
