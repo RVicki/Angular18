3c0baa74ed0ae4427e79bd1f308e079b75047068
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2023-12-15 13:27:34 +0100

    Added db and working show data input

--- zodiacApp/src/app/app.component.ts
+++ zodiacApp/src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'zodiacApp';
+  inputName:string = '';
+  inputDate: string = '';
+  displayedData: boolean = false;
+
+  displayData() {
+    this.displayedData = true;
+  }
+ 
 }
