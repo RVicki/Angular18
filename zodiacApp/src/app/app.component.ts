fc3c066c46ba1e0e3da2323e6048b9a986a6632f
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2023-12-18 11:24:30 +0100

    images added

--- zodiacApp/src/app/app.component.ts
+++ zodiacApp/src/app/app.component.ts
@@ -14,10 +14,66 @@ export class AppComponent {
   title = 'zodiacApp';
   inputName:string = '';
   inputDate: string = '';
+  foundZodiac:string = '';
   displayedData: boolean = false;
+  animalType:string = '';
+  url:string = 'http://localhost:3000/zodiac'
+  zodiacs:any[] = [];
 
   displayData() {
     this.displayedData = true;
+    this.fetchMyData();
+    this.findZodiac();
+  }
+
+  findZodiac() {
+    if(this.inputDate) {
+      const selectedYear = new Date(this.inputDate).getFullYear().toString();
+
+      const foundZodiac = this.zodiacs.find(
+        (zodiac) => zodiac.years.includes(selectedYear)
+      );
+
+      if(foundZodiac) {
+        this.foundZodiac = foundZodiac.animal;
+      } else {
+        this.foundZodiac = 'Zodiac not found for this year';
+      }
+    } else {
+      this.foundZodiac = 'Please select a valid date';
+    }
+  }
+
+  postData() {
+    const options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.3.0'},
+      body: JSON.stringify({
+        'animal': this.animalType,
+      })
+    };
+
+    fetch('http://localhost:3000/zodiac', options)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        this.fetchMyData();
+        this.animalType= '';
+      })
+      .catch(err => console.error(err));
+  }
+
+  fetchMyData() {
+    fetch(this.url)
+      .then(response => response.json())
+      .then(json => this.zodiacs = json)
+      .catch(err => console.error(err))
+  }
+
+  ngOnInit() { 
+
+    this.fetchMyData();
+
   }
  
 }
