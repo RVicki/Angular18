9009512ee21af684629a016020e4216a8a17f004
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2023-12-20 09:51:47 +0100

    added basics and register component

--- recap/src/app/app.component.ts
+++ recap/src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'recap';
+  title:string = 'recap of our angular course';
+  name:string = '';
+  myElements:Array<any> = [
+    {name: 'Suzie', id: 1},
+    {name: 'Bart', id: 2},
+    {name: 'Leo', id: 3}
+
+  ];
+
+  methodName() {
+    console.log('hello');
+  }
 }
